7d782867236a3065826204d36213078a3ba73227
Lukas Anda <lukas.anda@gmail.com>
2018-08-13 12:10:15 +0200

    Refactor settings

--- app/src/main/java/dev/dworks/apps/anexplorer/setting/SettingsActivity.java
+++ app/src/main/java/dev/dworks/apps/anexplorer/setting/SettingsActivity.java
@@ -17,7 +17,6 @@
 
 package dev.dworks.apps.anexplorer.setting;
 
-import android.annotation.TargetApi;
 import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
@@ -26,16 +25,13 @@ import android.graphics.Color;
 import android.graphics.drawable.ColorDrawable;
 import android.graphics.drawable.Drawable;
 import android.graphics.drawable.TransitionDrawable;
-import android.os.Build;
 import android.os.Bundle;
 import android.os.Handler;
 import android.preference.PreferenceManager;
-import androidx.core.content.ContextCompat;
 import android.text.TextUtils;
 import android.view.MenuItem;
 
-import java.util.List;
-
+import androidx.core.content.ContextCompat;
 import dev.dworks.apps.anexplorer.DocumentsApplication;
 import dev.dworks.apps.anexplorer.R;
 import dev.dworks.apps.anexplorer.misc.AnalyticsManager;
@@ -45,11 +41,11 @@ import dev.dworks.apps.anexplorer.misc.SystemBarTintManager;
 import dev.dworks.apps.anexplorer.misc.Utils;
 
 public class SettingsActivity extends AppCompatPreferenceActivity {
-
+    
     public static final String TAG = "Settings";
-
+    
     private static final int FRAGMENT_OPEN = 99;
-
+    
     private static final String EXTRA_RECREATE = "recreate";
     public static final String KEY_ADVANCED_DEVICES = "advancedDevices";
     public static final String KEY_FILE_SIZE = "fileSize";
@@ -65,23 +61,23 @@ public class SettingsActivity extends AppCompatPreferenceActivity {
     public static final String KEY_THEME_STYLE = "themeStyle";
     public static final String KEY_FOLDER_ANIMATIONS = "folderAnimations";
     public static final String KEY_RECENT_MEDIA = "recentMedia";
-
-	private Resources res;
-	private int actionBarColor;
+    
+    private Resources res;
+    private int actionBarColor;
     private final Handler handler = new Handler();
     private Drawable oldBackground;
     private boolean mRecreate = false;
-
+    
     public static boolean getDisplayAdvancedDevices(Context context) {
         return PreferenceManager.getDefaultSharedPreferences(context)
                 .getBoolean(KEY_ADVANCED_DEVICES, true);
     }
-
+    
     public static boolean getDisplayFileSize(Context context) {
         return PreferenceManager.getDefaultSharedPreferences(context)
                 .getBoolean(KEY_FILE_SIZE, true);
     }
-
+    
     public static boolean getDisplayFolderSize(Context context) {
         return PreferenceManager.getDefaultSharedPreferences(context)
                 .getBoolean(KEY_FOLDER_SIZE, false);
@@ -91,50 +87,55 @@ public class SettingsActivity extends AppCompatPreferenceActivity {
         return PreferenceManager.getDefaultSharedPreferences(context)
                 .getBoolean(KEY_FILE_THUMBNAIL, true);
     }
-
+    
     public static boolean getDisplayFileHidden(Context context) {
         return PreferenceManager.getDefaultSharedPreferences(context)
                 .getBoolean(KEY_FILE_HIDDEN, false);
     }
-
+    
     public static boolean getDisplayRecentMedia() {
-        return PreferenceManager.getDefaultSharedPreferences(DocumentsApplication.getInstance().getBaseContext())
+        return PreferenceManager.getDefaultSharedPreferences(DocumentsApplication.getInstance()
+                .getBaseContext())
                 .getBoolean(KEY_RECENT_MEDIA, true);
     }
-
+    
     public static boolean getRootMode(Context context) {
         return PreferenceManager.getDefaultSharedPreferences(context)
                 .getBoolean(KEY_ROOT_MODE, true);
     }
     
     public static int getPrimaryColor(Context context) {
-    	int newColor = ContextCompat.getColor(context, R.color.primaryColor);
+        int newColor = ContextCompat.getColor(context, R.color.primaryColor);
         return PreferenceManager.getDefaultSharedPreferences(context)
                 .getInt(KEY_PRIMARY_COLOR, newColor);
     }
-
+    
     public static int getPrimaryColor() {
-        return PreferenceManager.getDefaultSharedPreferences(DocumentsApplication.getInstance().getBaseContext())
+        return PreferenceManager.getDefaultSharedPreferences(DocumentsApplication.getInstance()
+                .getBaseContext())
                 .getInt(KEY_PRIMARY_COLOR, Color.parseColor("#0288D1"));
     }
-
+    
     public static int getAccentColor() {
-        return PreferenceManager.getDefaultSharedPreferences(DocumentsApplication.getInstance().getBaseContext())
+        return PreferenceManager.getDefaultSharedPreferences(DocumentsApplication.getInstance()
+                .getBaseContext())
                 .getInt(KEY_ACCENT_COLOR, Color.parseColor("#EF3A0F"));
     }
-
+    
     public static void setAccentColor(int color) {
-        SharedPreferences preferences = PreferenceManager.getDefaultSharedPreferences(DocumentsApplication.getInstance().getBaseContext());
+        SharedPreferences preferences = PreferenceManager.getDefaultSharedPreferences
+                (DocumentsApplication.getInstance().getBaseContext());
         SharedPreferences.Editor editor = preferences.edit();
         editor.putInt(KEY_ACCENT_COLOR, color);
-        editor.commit();
+        editor.apply();
     }
-
+    
     public static String getThemeStyle() {
-        return PreferenceManager.getDefaultSharedPreferences(DocumentsApplication.getInstance().getBaseContext())
+        return PreferenceManager.getDefaultSharedPreferences(DocumentsApplication.getInstance()
+                .getBaseContext())
                 .getString(KEY_THEME_STYLE, "1");
     }
-
+    
     public static void setThemeStyle(int style) {
         PreferenceUtils.set(KEY_THEME_STYLE, String.valueOf(style));
     }
@@ -147,18 +148,20 @@ public class SettingsActivity extends AppCompatPreferenceActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-
+        
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
         changeActionBarColor(0);
         res = getResources();
         actionBarColor = getPrimaryColor(this);
+        getFragmentManager().beginTransaction().replace(android.R.id.content, new
+                SettingsPreferenceFragment()).commit();
     }
 
-	@Override
-	public void onBuildHeaders(List<Header> target) {
-		loadHeadersFromResource(R.xml.pref_headers, target);
-	}
-
+//	@Override
+//	public void onBuildHeaders(List<Header> target) {
+//		loadHeadersFromResource(R.xml.pref_headers, target);
+//	}
+    
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
         if (item.getItemId() == android.R.id.home) {
@@ -170,25 +173,28 @@ public class SettingsActivity extends AppCompatPreferenceActivity {
     
     @Override
     protected boolean isValidFragment(String fragmentName) {
-    	return true;
+        return true;
     }
     
-	public static final boolean isPinEnabled(Context context) {
-        return PreferenceManager.getDefaultSharedPreferences(context).getBoolean(KEY_PIN_ENABLED, false)
-        		&& isPinProtected(context);
+    public static boolean isPinEnabled(Context context) {
+        return PreferenceManager.getDefaultSharedPreferences(context).getBoolean(KEY_PIN_ENABLED,
+                false)
+                && isPinProtected(context);
     }
-	
-    public static final boolean isPinProtected(Context context) {
+    
+    public static boolean isPinProtected(Context context) {
         return PreferenceManager.getDefaultSharedPreferences(context).getString(KEY_PIN, "") != "";
     }
     
     public static void setPin(Context context, String pin) {
-    	PreferenceManager.getDefaultSharedPreferences(context).edit().putString(KEY_PIN, hashKeyForPIN(pin)).commit();
+        PreferenceManager.getDefaultSharedPreferences(context).edit().putString(KEY_PIN,
+                hashKeyForPIN(pin)).apply();
     }
     
     public static boolean checkPin(Context context, String pin) {
         pin = hashKeyForPIN(pin);
-        String hashed = PreferenceManager.getDefaultSharedPreferences(context).getString(KEY_PIN, "");
+        String hashed = PreferenceManager.getDefaultSharedPreferences(context).getString(KEY_PIN,
+                "");
         if (TextUtils.isEmpty(pin))
             return TextUtils.isEmpty(hashed);
         return pin.equals(hashed);
@@ -200,8 +206,7 @@ public class SettingsActivity extends AppCompatPreferenceActivity {
         try {
             //MessageDigest digester = MessageDigest.getInstance("MD5");
             //return Base64.encodeToString(value.getBytes(), Base64.DEFAULT);
-        }
-        catch (Exception e) {
+        } catch (Exception e) {
             CrashReportingManager.logException(e);
         }
         return value;
@@ -222,83 +227,83 @@ public class SettingsActivity extends AppCompatPreferenceActivity {
     
     @Override
     protected void onResume() {
-    	super.onResume();
+        super.onResume();
         changeActionBarColor(0);
     }
-
-
+    
+    
     @Override
     public void startActivity(Intent intent) {
         super.startActivityForResult(intent, FRAGMENT_OPEN);
     }
-
+    
     @Override
     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
         super.onActivityResult(requestCode, resultCode, data);
-        if(requestCode == FRAGMENT_OPEN){
-            if(resultCode == RESULT_FIRST_USER){
+        if (requestCode == FRAGMENT_OPEN) {
+            if (resultCode == RESULT_FIRST_USER) {
                 recreate();
             }
         }
     }
-
+    
     @Override
     public void recreate() {
         mRecreate = true;
         super.recreate();
     }
-
+    
     @Override
     public String getTag() {
         return TAG;
     }
-
+    
     @Override
     protected void onSaveInstanceState(Bundle outState) {
         outState.putBoolean(EXTRA_RECREATE, true);
         super.onSaveInstanceState(outState);
     }
-
+    
     @Override
     protected void onRestoreInstanceState(Bundle state) {
         super.onRestoreInstanceState(state);
-        if(state.getBoolean(EXTRA_RECREATE)){
+        if (state.getBoolean(EXTRA_RECREATE)) {
             setResult(RESULT_FIRST_USER);
         }
     }
-
+    
     public void changeActionBarColor(int newColor) {
-
-		int color = newColor != 0 ? newColor : SettingsActivity.getPrimaryColor(this);
-		Drawable colorDrawable = new ColorDrawable(color);
-
-		if (oldBackground == null) {
+        
+        int color = newColor != 0 ? newColor : SettingsActivity.getPrimaryColor(this);
+        Drawable colorDrawable = new ColorDrawable(color);
+        
+        if (oldBackground == null) {
             getSupportActionBar().setBackgroundDrawable(colorDrawable);
-
+            
         } else {
-			TransitionDrawable td = new TransitionDrawable(new Drawable[] { oldBackground, colorDrawable });
+            TransitionDrawable td = new TransitionDrawable(new Drawable[]{oldBackground,
+                    colorDrawable});
             getSupportActionBar().setBackgroundDrawable(td);
-			td.startTransition(200);
-		}
-
-		oldBackground = colorDrawable;
+            td.startTransition(200);
+        }
+        
+        oldBackground = colorDrawable;
         setUpStatusBar();
-	}
-
+    }
+    
     public void setUpStatusBar() {
         int color = Utils.getStatusBarColor(SettingsActivity.getPrimaryColor(this));
-        if(Utils.hasLollipop()){
+        if (Utils.hasLollipop()) {
             getWindow().setStatusBarColor(color);
-        }
-        else if(Utils.hasKitKat()){
+        } else if (Utils.hasKitKat()) {
             SystemBarTintManager systemBarTintManager = new SystemBarTintManager(this);
             systemBarTintManager.setTintColor(color);
             systemBarTintManager.setStatusBarTintEnabled(true);
         }
     }
-
-	public static void logSettingEvent(String key){
-        AnalyticsManager.logEvent("settings_"+key.toLowerCase());
+    
+    public static void logSettingEvent(String key) {
+        AnalyticsManager.logEvent("settings_" + key.toLowerCase());
     }
-
+    
 }
